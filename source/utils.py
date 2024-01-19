319af2d54b1323a56bb4b0558909c0e1f30b70ef
tomhosker <tomdothosker@gmail.com>
2024-01-19 16:14:56 +0000

    Removed most messages.

--- source/utils.py
+++ source/utils.py
@@ -26,57 +26,40 @@ class GetReturnCode(Enum):
 # FUNCTIONS #
 #############
 
-def run_mega_command(second_half, args=None):
+def run_mega_command(second_half, args=None, quiet=True):
     """ Run a shell command beginning with `mega-`. """
     commands = ["mega-"+second_half]
+    local_stdout = subprocess.STDOUT
     if args:
         commands += args
+    if quiet:
+        local_stdout = subprocess.DEVNULL
     try:
-        subprocess.run(commands, check=True)
+        subprocess.run(commands, check=True, stdout=local_stdout)
     except subprocess.CalledProcessError:
         return False
     return True
 
-def report(success):
-    """ Report on whether a given Mega command succeeded or not. """
-    if success:
-        print("Done.")
-    else:
-        print("Failed.")
-
-def mega_login(username: str) -> bool:
+def mega_login(username: str, quiet: bool = True) -> bool:
     """ Ronseal. """
     password = getpass(prompt="Enter your Mega password to log in: ")
-    print("Logging in...")
-    success = run_mega_command("login", [username, password])
-    report(success)
-    return success
+    return run_mega_command("login", [username, password], quiet=quiet)
 
-def mega_logout() -> bool:
+def mega_logout(quiet: bool = True) -> bool:
     """ Ronseal. """
-    success = run_mega_command("logout")
-    report(success)
-    return success
+    return run_mega_command("logout", quiet=quiet)
 
-def mega_sync(local_path: str, remote_path: str) -> bool:
+def mega_sync(local_path: str, remote_path: str, quiet: bool = True) -> bool:
     """ Sync a given remote folder to its local equivalent. """
     Path(local_path).mkdir(parents=True, exist_ok=True)
-    print(
-        "Syncing remote folder "+
-        remote_path+
-        " to local folder "+
-        local_path+
-        "..."
-    )
-    success = run_mega_command("sync", [local_path, remote_path])
-    report(success)
-    return success
+    return run_mega_command("sync", [local_path, remote_path], quiet=quiet)
 
 def mega_get_dir(
         url_or_path: str,
         local_path: str,
         auto_delete: bool = False,
-        auto_keep: bool = False
+        auto_keep: bool = False,
+        quiet: bool = True
     ) -> GetReturnCode:
     """ Download a directory at the above URL or remote path to the above local
     path. """
@@ -93,26 +76,33 @@ def mega_get_dir(
         if auto_keep:
             return GetReturnCode.PRESENT
         if auto_delete or get_yes_no(message):
-            print("Deleting...")
+            if not quiet:
+                print("Deleting...")
             shutil.rmtree(local_path)
         else:
+            if not quiet:
+                print(local_path+" already exists.")
             return GetReturnCode.PRESENT
     local_path_obj.mkdir(parents=True)
-    success = run_mega_command("get", ["-m", url_or_path, local_path])
-    report(success)
+    success = \
+        run_mega_command("get", ["-m", url_or_path, local_path], quiet=quiet)
     if success:
         return GetReturnCode.SUCCESS
     return GetReturnCode.FAILURE
 
-def mega_get(url_or_path: str, local_path: str) -> GetReturnCode:
+def mega_get(
+        url_or_path: str,
+        local_path: str,
+        quiet: bool = True
+    ) -> GetReturnCode:
     """ Download a file at the above URL or remote path to the above local
     path. """
     local_path_obj = Path(local_path)
     if local_path_obj.exists():
-        print(local_path+" already exists.")
+        if not quiet:
+            print(local_path+" already exists.")
         return GetReturnCode.PRESENT
-    success = run_mega_command("get", [url_or_path, local_path])
-    report(success)
+    success = run_mega_command("get", [url_or_path, local_path], quiet=quiet)
     if success:
         return GetReturnCode.SUCCESS
     return GetReturnCode.FAILURE
